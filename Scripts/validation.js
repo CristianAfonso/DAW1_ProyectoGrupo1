cf128e1a6ee0773cc8dfcd21cae76ac907be0885
Tigotan <Tigotan.jimenez@gmail.com>
2021-11-10 12:30:10 +0000

    Problemas con la validacion

--- Scripts/validation.js
+++ Scripts/validation.js
@@ -1,54 +1,32 @@
 
-const contacto = ['nombre','correo','telefono'];
-const paginas = ['contacto', 'register', 'login'];
 
 
-var path = window.location.pathname;
-var page = path.split("/").pop();
-page = page.split('.').shift();
+$(document). ready(function (){
 
 
- function pagina(page) {
-     let i = 0;
-     let attributeValue;
-     if( page === paginas[0]){
-        for( a in contacto){
-            console.log(a);
-            attributeValue = document.getElementsByTagName("INPUT")[i].getAttribute("pattern");
-            if(document.getElementById(a).validity.patternMismatch == true) {
-                document.getElementById(a)
-                    .setCustomValidity('Este campo debe atender a ' + attributeValue);
-                i++;
+    const contacto = ['nombre','correo','telefono'];
+    const paginas = ['contacto', 'register', 'login'];
+
+
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    page = page.split('.').shift();
+
+    $("#env").bind("click", function(){
+        let i = 0;
+        let attributeValue;
+        if( page === paginas[0]){
+            console.log("que hace");
+            for( i = 0; i < contacto.length; i++){
+                console.log(contacto[i]);
+                attributeValue = document.getElementById(contacto[i]).getAttribute("pattern");
+                if(document.getElementById(contacto[i]).validity.patternMismatch == true) {
+                    document.getElementById(contacto[i]).setCustomValidity('Este campo debe atender a ' + attributeValue);
+                }
             }
         }
-        i = 0;
-    }
+    });
 
-    if( page === paginas[1]){
-        for( a in contacto){
-            console.log(a);
-            attributeValue = document.getElementsByTagName("INPUT")[i].getAttribute("pattern");
-            if(document.getElementById(a).validity.patternMismatch == true) {
-                document.getElementById(a)
-                    .setCustomValidity('Este campo debe atender a ' + attributeValue);
-                i++;
-            }
-        }
-        i = 0;
-    }
 
-    if( page === paginas[2]){
-        for( a in contacto){
-            console.log(a);
-            attributeValue = document.getElementsByTagName("INPUT")[i].getAttribute("pattern");
-            if(document.getElementById(a).validity.patternMismatch == true) {
-                document.getElementById(a)
-                    .setCustomValidity('Este campo debe atender a ' + attributeValue);
-                i++;
-            }
-        }
-        i = 0;
-    }
-}
+});
 
-pagina(page);
