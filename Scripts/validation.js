e22a7fda2ae4e50792c7fffc81e56612a6aa64d1
Tigotan Jiménez Quintana <Tigotan.Jimenez@gmail.com>
2021-11-13 13:00:40 +0000

    Cambios a Validacion y Responsiveness del Index.html

--- Scripts/validation.js
+++ Scripts/validation.js
@@ -5,7 +5,9 @@ $(document). ready(function (){
 
 
     const contacto = ['nombre','correo','telefono'];
-    const paginas = ['contacto', 'register', login];
+    const login = ['Uname', 'Pass', 'check'];
+    const register = ['Name', 'Surname', 'Phone', 'Email', 'Pass', 'Pass2'];
+    const paginas = ['contacto', 'register', 'login'];
 
 
     var path = window.location.pathname;
@@ -17,6 +19,7 @@ $(document). ready(function (){
         let attributeValue;
         let validityState;
         let inputId;
+        let passwd1;
         if( page === paginas[0]){
             for( i = 0; i < contacto.length; i++){
                 input = document.getElementById(contacto[i]);
@@ -40,21 +43,48 @@ $(document). ready(function (){
         }
 
         if( page === paginas[1]){
-            for( i = 0; i < contacto.length; i++){
-                input = document.getElementById(contacto[i]);
-                attributeValue = document.getElementById(contacto[i]).getAttribute("pattern");
+            for( i = 0; i < register.length; i++){
+                input = document.getElementById(register[i]);
+                attributeValue = document.getElementById(register[i]).getAttribute("pattern");
                 validityState = input.validity;
+                inputId = input.getAttribute("id");
+                /* if(inputId == 'Pass2' ){
+                    passwd1 = document.getElementById("Pass").getAttribute("value"); 
+                    $('Pass2').attr('pattern', ''+passwd1);
+                } */
                 if (validityState.tooLong){
                     input.setCustomValidity('El campo no puede superar los ' + input.getAttribute('maxlength') + 'caracteres');
-                    $(input).css("border", "red solid 1px");
+                    $("#"+inputId).css("border", "red solid 1px");
                 } else if (validityState.tooShort){
                     input.setCustomValidity('El campo debe superar los ' + input.getAttribute('minlength') + ' caracteres');
-                    $(input).css("border", "red solid 1px");
+                    $("#"+inputId).css("border", "red solid 1px");
                 } else if (validityState.patternMismatch){
                     input.setCustomValidity('El campo no cuadra con las posibilidades');
-                    $(input).css("border", "red solid 1px");
+                    $("#"+inputId).css("border", "red solid 1px");
                 }else{
-                    $(input).css('border', '');
+                    $("#"+inputId).css('border', '');
+                    input.setCustomValidity('');
+                }
+            }
+        }
+
+        if( page === paginas[2]){
+            for( i = 0; i < login.length; i++){
+                input = document.getElementById(login[i]);
+                attributeValue = document.getElementById(login[i]).getAttribute("pattern");
+                validityState = input.validity;
+                inputId = input.getAttribute("id");
+                if (validityState.tooLong){
+                    input.setCustomValidity('El campo no puede superar los ' + input.getAttribute('maxlength') + 'caracteres');
+                    $("#"+inputId).css("border", "red solid 1px");
+                } else if (validityState.tooShort){
+                    input.setCustomValidity('El campo debe superar los ' + input.getAttribute('minlength') + ' caracteres');
+                    $("#"+inputId).css("border", "red solid 1px");
+                } else if (validityState.patternMismatch){
+                    input.setCustomValidity('El campo no cuadra con las posibilidades');
+                    $("#"+inputId).css("border", "red solid 1px");
+                }else{
+                    $("#"+inputId).css('border', '');
                     input.setCustomValidity('');
                 }
             }
