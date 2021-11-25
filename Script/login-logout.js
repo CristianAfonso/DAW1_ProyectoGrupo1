a8f9f193f5f764b831a76062d3315287bd3b6d23
can97 <can97moya@gmail.com>
2021-11-25 18:01:50 +0000

    nav ahora tiene acceso a perfil cuando logged

--- Script/login-logout.js
+++ Script/login-logout.js
@@ -1,25 +1,25 @@
 var cuarto = document.querySelectorAll('#cuarto,#cuartoMenu');
 var sexto = document.querySelectorAll('#sexto,#sextoMenu');
 $(cuarto).click(function login() {
-    document.getElementById("menuLinks").style.height = "15rem";
-    document.getElementById("menuLinks").style.gridAutoRows="20%";
     document.getElementById("cuarto").style.display = "none";
     document.getElementById("quinto").style.display = "none";
     document.getElementById("sexto").style.display= "flex";
+    document.getElementById("septimo").style.display= "flex";
     document.getElementById("cuartoMenu").style.display = "none";
     document.getElementById("quintoMenu").style.display = "none";
     document.getElementById("quintoMenu").style.gridRow = "grid-row: 4";
     document.getElementById("sextoMenu").style.display= "flex";
+    document.getElementById("septimoMenu").style.display= "flex";
 });
 
 $(sexto).click(function logout() {
-    document.getElementById("menuLinks").style.height = "20rem";
-    document.getElementById("menuLinks").style.gridAutoRows="15%";
     document.getElementById("cuarto").style.display = "flex";
     document.getElementById("quinto").style.display = "flex";
     document.getElementById("sexto").style.display = "none";
+    document.getElementById("septimo").style.display= "none";
     document.getElementById("cuartoMenu").style.display = "flex";
     document.getElementById("quintoMenu").style.display = "flex";
     document.getElementById("quintoMenu").style.gridRow = "grid-row: 5";
     document.getElementById("sextoMenu").style.display= "none";
+    document.getElementById("septimoMenu").style.display= "none";
 });
