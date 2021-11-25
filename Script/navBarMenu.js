718d8048d6d4900866f0007441eb837c2e87c3fe
can97 <can97moya@gmail.com>
2021-11-25 17:49:21 +0000

    Nav-menu ahora se cierra cuando pasa a tablet asique a la vuelta no está abierto, QoL

--- Script/navBarMenu.js
+++ Script/navBarMenu.js
@@ -6,3 +6,9 @@ $("#menu").click(function menuDesplegable() {
         x.style.display = "grid";
     }
 });
+
+window.addEventListener( "resize", function(){
+    if(document.documentElement.clientWidth>768){
+        document.getElementById("menuLinks").style.display="none";
+    }
+}, true)
