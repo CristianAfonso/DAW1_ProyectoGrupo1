6e3233eafc6c23cceb8a97ba148d784efd275939
can97 <can97moya@gmail.com>
2021-12-16 13:36:36 +0000

    perfil avanzado

--- Scripts/perfilScript.js
+++ Scripts/perfilScript.js
@@ -3,14 +3,26 @@ $("#perfil").click(function activaPerfil() {
     document.getElementById("centerPerf").style.display= "grid";
     document.getElementById("activ").style.display= "none";
     document.getElementById("contTable").style.display= "none";
+    document.getElementById("perfil").style.color= "#e03315";
+    document.getElementById("conf").style.color= "#424242";
+    document.getElementById("actividades").style.color= "#424242";
+    document.getElementById("horario").style.color= "#424242";
 });
 $("#actividades").click(function activaActiv() {
     document.getElementById("centerPerf").style.display= "none";
     document.getElementById("activ").style.display= "grid";
     document.getElementById("contTable").style.display= "none";
+    document.getElementById("perfil").style.color= "#424242";
+    document.getElementById("conf").style.color= "#424242";
+    document.getElementById("actividades").style.color= "#e03315";
+    document.getElementById("horario").style.color= "#424242";
 });
 $("#horario").click(function activaHorario() {
     document.getElementById("centerPerf").style.display= "none";
     document.getElementById("activ").style.display= "none";
     document.getElementById("contTable").style.display= "flex";
+    document.getElementById("perfil").style.color= "#424242";
+    document.getElementById("conf").style.color= "#424242";
+    document.getElementById("actividades").style.color= "#424242";
+    document.getElementById("horario").style.color= "#e03315";
 });
